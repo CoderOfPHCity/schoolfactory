0dae67ae29bbf21003a93ce1d8571d688280c6d4
Daniel from Phcity <103477809+CoderOfPHCity@users.noreply.github.com>
2024-02-09 17:09:27 +0100

    Update Factory.sol

--- Factory.sol
+++ Factory.sol
@@ -4,42 +4,19 @@ pragma solidity ^0.8;
 import "./Principal.sol";
 import "./Teacher.sol";
 
-contract Factory{
-
-    constructor(){
-        funcDeploy();
-
-    }
-
-          struct Student {
-        string name;
-        uint256 matNumber;
-        uint256 scores;
-    }
-      Student[] public student;
+contract Factory {
 
 
-    Teacher public teacher;
-    Principal public principal;
+constructor(){
+    createSchool();
 
-    function funcDeploy() public {
-        teacher = new Teacher();
-        principal = new Principal(); 
-
-
-    }
-
-     function getScoreSheet() public view returns (Student[] memory) {
-        return student;
-    }
-
-    function updateScores(uint256 id, uint256 Score) public {
-    require(id < student.length, "Invalid student index");
-    teacher.updateScore(id, Score);
-    require(teacher.teachers(msg.sender) || principal.principalAddress() ==msg.sender, "Unauthorized");
-    student[id].scores = Score;
 }
+    Teacher public teachers;
+    Principal public principals;
 
+    function createSchool() public {
+        principals = new Principal();
+        teachers = new Teacher();
 
-
+    }
 }
