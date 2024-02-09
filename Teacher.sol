1635f701e98b627438f9a4c73788a8f362975fbb
Daniel from Phcity <103477809+CoderOfPHCity@users.noreply.github.com>
2024-02-09 17:11:09 +0100

    Update Teacher.sol

--- Teacher.sol
+++ Teacher.sol
@@ -1,15 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-contract Teacher{
-      mapping(address => bool) public teachers;
+
+contract Teacher {
+
+     struct Student {
+        string name;
+        uint256 matNumber;
+        uint256 scores;
+    }
+
+     mapping(address => bool) public teachers;
+      Student[] public student;
 
          function updateScore(uint256 id, uint256 _score) public {
-        // require(
-        //     teachers[msg.sender] || principal == msg.sender,
-        //     "wida you!, no trespassing is allowed"
-        // );
-        // Student memory studentscore = student[id];
-        // studentscore.scores = _score;
+        require(
+            teachers[msg.sender],
+            "wida you!, no trespassing is allowed"
+        );
+        Student storage studentscore = student[id];
+        studentscore.scores = _score;
     }
 }
