bba253caa2a1e3106c0f1bda5f6c3f88847dbe6c
Daniel from Phcity <103477809+CoderOfPHCity@users.noreply.github.com>
2024-02-10 16:03:14 +0100

    Update Teacher.sol

--- Teacher.sol
+++ Teacher.sol
@@ -1,17 +1,15 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+ // SPDX-License-Identifier: MIT
+ pragma solidity ^0.8;
 
-import "./Principal.sol";
+ import "./Principal.sol";
 
 contract Teacher {
-    function updateScore(uint256 id, uint256 _score) public {
-        // require(
-        //     Principal.principalAddress(msg.sender),
-        //     //  || IPrincipal(msg.sender).teachers,
-        //     "wida you!, no trespassing is allowed"
-        // );
+     PrincipalInterface principalContract;
 
-        Principal principal = Principal(msg.sender);
-        principal.updateScores(id, _score);
+    constructor(address  _principalContract) {
+        principalContract = PrincipalInterface(_principalContract);
+    }
+    function updateStudentScore(uint256 id, uint256 score) external {
+        principalContract.updateScores(id, score);
     }
 }
